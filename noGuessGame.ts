9a6f847f03bd5741125d0c61ae0de6eec561274b
JalilUmar <125781892+JalilUmar@users.noreply.github.com>
2023-03-09 07:47:13 +0500

    Add files via upload

---
+++ noGuessGame.ts
@@ -0,0 +1,27 @@
+
+
+import inquirer from "inquirer"
+
+type ansType = {
+    userGuess: number
+}
+const systemGeneratedNo = Math.floor(Math.random())
+
+let answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Write your guess b/w 1 to 10: "
+    }
+])
+
+const {userGuess} = answers ;
+console.log(userGuess , "UserGuess" , systemGeneratedNo , "SYs")
+if(userGuess === systemGeneratedNo)
+{
+    console.log("Your amswer is correct. \n You Win !");
+    
+}else{
+    console.log("Please try again Better luck next time.");
+    
+}
